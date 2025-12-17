98d079ac3dccd4d19bde72fd8f683eb9e5132143
Quinten <quinten@bumbal.eu>
2025-12-17 23:41:33 +0100

    chore(laravel): format style pint #37

--- services/auth-service/src/database/seeders/DatabaseSeeder.php
+++ services/auth-service/src/database/seeders/DatabaseSeeder.php
@@ -23,13 +23,12 @@ class DatabaseSeeder extends Seeder
             'email' => 'quinten@bumbal.eu',
         ]);
 
-        if (!app()->environment('local')) {
+        if (! app()->environment('local')) {
             return;
         }
 
         $clientId = env('OAUTH_DEV_CLIENT_ID', '019b2d20-ce15-7335-828a-b184b656c035');
         $redirects = env('OAUTH_DEV_REDIRECT', 'http://localhost:8080/callback');
-
 
         DB::table('oauth_clients')->insert([
             'id' => $clientId,
@@ -38,7 +37,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Frontend Service',
             'secret' => null,
             'provider' => null,
-            'redirect_uris' => '[' . implode(',', array_map(fn($url) => '"' . trim($url) . '"', explode(',', $redirects))) . ']',
+            'redirect_uris' => '['.implode(',', array_map(fn ($url) => '"'.trim($url).'"', explode(',', $redirects))).']',
             'grant_types' => '["authorization_code", "refresh_token"]',
             'revoked' => false,
             'created_at' => now(),
