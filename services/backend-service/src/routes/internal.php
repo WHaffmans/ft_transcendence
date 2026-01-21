6041a9259743db24336ef98b7f805dae723a6083
whaffman <whaffman@student.codam.nl>
2026-01-21 18:35:01 +0100

    feat: make real dev docker and add internal routes #61

--- services/backend-service/src/routes/internal.php
+++ services/backend-service/src/routes/internal.php
@@ -9,4 +9,9 @@ Route::middleware('auth.internal')->group(function () {
 		return response()->json(['message' => 'Internal route accessed'], 200);
 	});
 
+	Route::post('/games/{game}/finish', [App\Http\Controllers\GameController::class, 'finishGame']);
+    Route::post('/games/{game}/start', [App\Http\Controllers\GameController::class, 'startGame']);
+    Route::post('/games/{game}/leave', [App\Http\Controllers\GameController::class, 'leaveGame']);
+	Route::get('/games/{game}', [App\Http\Controllers\GameController::class, 'show']);
+
 });
