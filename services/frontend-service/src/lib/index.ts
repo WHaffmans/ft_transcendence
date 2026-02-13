09c1bd7225584cd38253d30c7e49f057e73e69d2
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2026-02-13 12:40:21 +0100

    chore(frontend): remove unused barrel file [index.ts] #107

--- services/frontend-service/src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-// Component exports
-export { default as LeaderboardEntry } from './components/LeaderboardEntry.svelte';
-export { default as Leaderboard } from './components/Leaderboard.svelte';
-export { default as LoginSection } from './components/LoginSection.svelte';
-export { default as Footer } from './components/Footer.svelte';
