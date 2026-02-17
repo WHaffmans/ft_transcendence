0c8d7487b706bb52515f7eae69ee718b0936956c
QBeukelman <quentin@nextresearcg@gmail.com>
2026-02-17 08:09:58 +0100

    feat(game-service): send x new segments #42

--- services/game-service/src/app/room_manager.ts
+++ services/game-service/src/app/room_manager.ts
@@ -6,7 +6,7 @@
 /*   By: quentinbeukelman <quentinbeukelman@stud      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/01/06 14:35:21 by quentinbeuk   #+#    #+#                 */
-/*   Updated: 2026/02/15 14:44:11 by quentinbeuk   ########   odam.nl         */
+/*   Updated: 2026/02/17 08:07:45 by quentinbeuk   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -602,6 +602,10 @@ export class RoomManager {
 	 * Make snapshot with correct types
 	 */
 	private makeSnapshot(room: Room) {
+
+		const segs = room.state.segments;
+		const start = Math.max(0, segs.length = room.config.segmentSendCount);
+
 		return {
 			phase: room.phase,
 			tick: room.state.tick,
@@ -622,7 +626,7 @@ export class RoomManager {
 				color: p.color,
 			})),
 
-			segments: room.state.segments.map((s) => ({
+			segments: segs.slice(start).map((s) => ({
 				x1: s.x1,
 				y1: s.y1,
 				x2: s.x2,
