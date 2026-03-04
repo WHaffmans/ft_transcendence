da0347539e657d4b18ae17fcec88629dc25be4cc
QBeukelman <quentin.nextresearch@gmail.com>
2026-03-04 17:56:30 +0100

    feat(game-service): optimise start pos #210

--- services/game-service/src/engine/config.ts
+++ services/game-service/src/engine/config.ts
@@ -6,7 +6,7 @@
 /*   By: qbeukelm <qbeukelm@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/12/16 11:12:05 by qbeukelm      #+#    #+#                 */
-/*   Updated: 2026/03/04 17:46:41 by quentinbeuk   ########   odam.nl         */
+/*   Updated: 2026/03/04 17:53:17 by quentinbeuk   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,9 @@ export type GameConfig = {
 	gapChance: number;				// probability per tick to START a gap (0..1)
 	gapMinTicks: number;
 	gapMaxTicks: number;
+
+	spawnPadding: number,
+	spawnAngle: number,
 };
 
 // Set default values
